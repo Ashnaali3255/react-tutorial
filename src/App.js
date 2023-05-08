baceb70c34d2b45460c6a8ae593182d39236813a
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-08 20:11:57 +0500

    Update .gitignore file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoList from './Component/todo';
 
 function App() {
   return (
     <div className="App">
-      
+      <TodoList/>
     </div>
   );
 }
---
+++ src/Component/todo.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    setTodos([...todos, { text: newTodo, completed: false }]);
+    setNewTodo('');
+  };
+
+  const handleToggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={newTodo} onChange={handleNewTodoChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleTodo(index)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
